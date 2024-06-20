6b0e9bfaa826aee87262ae88899d8d667e9d5af2
tetn39 <tetn39@gmail.com>
2024-06-20 19:01:32 +0900

    add: abc173_b

---
+++ practice/abc173_b.py
@@ -0,0 +1,10 @@
+n = int(input())
+dic = {"AC": 0, "WA": 0, "TLE": 0, "RE": 0}
+for i in range(n):
+    s = input()
+    dic[s] += 1
+
+print("AC x", dic["AC"])
+print("WA x", dic["WA"])
+print("TLE x", dic["TLE"])
+print("RE x", dic["RE"])
